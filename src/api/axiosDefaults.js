7e5b691c4584137a4e2f4b5193f375cb432a59d8
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2024-12-30 17:18:26 +0000

    test

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://moments-proj-fc5321aa27e8.herokuapp.com/'
+axios.defaults.baseURL = 'https://content-sharing-platform-a296effdf373.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
